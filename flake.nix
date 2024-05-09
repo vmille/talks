0558bf60835b8c092a6396fdae88b710cb4397c9
Vivien MILLE <mille.vivien@proton.me>
2024-05-09 14:09:24 +0200

    adapting to nix flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,19 @@
 {
-  description = "A very basic flake";
+  description = "My talks";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { system = "${system}"; config.allowUnfree = true; };
+    in
+      {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ pkgs.git pkgs.nodejs pkgs.marp-cli pkgs.jetbrains.webstorm];
+          };
+      }
+    );
 }
--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {}}:
-
-pkgs.mkShell {
-  packages = [ pkgs.git pkgs.nodejs pkgs.jetbrains.webstorm ];
-  shellHook = ''
-    npm install @marp-team/marp-cli
-    '';
-}
